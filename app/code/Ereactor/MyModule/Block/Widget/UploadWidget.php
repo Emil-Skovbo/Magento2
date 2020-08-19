781e285425e7d66f98ecbb64498714bc7e94db4b
Emil-Skovbo <emilskovbo18@gmail.com>
2020-08-19 15:39:20 +0200

    making a custom widget for magento 2

---
+++ app/code/Ereactor/MyModule/Block/Widget/UploadWidget.php
@@ -0,0 +1,52 @@
+<?php
+namespace Ereactor\MyModule\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+
+
+
+class UploadWidget extends Template implements BlockInterface
+{
+    protected $_template = "widget/categoryIcons.phtml";
+    protected $cataRepo;
+
+
+    public function __construct(Template\Context $context, array $data = [], CategoryRepositoryInterface $cataRepo)
+{
+    $this->validator = $context->getValidator();
+    $this->resolver = $context->getResolver();
+    $this->_filesystem = $context->getFilesystem();
+    $this->templateEnginePool = $context->getEnginePool();
+    $this->_storeManager = $context->getStoreManager();
+    $this->_appState = $context->getAppState();
+    $this->templateContext = $this;
+    $this->pageConfig = $context->getPageConfig();
+    $this->cataRepo = $cataRepo;
+    parent::__construct($context, $data);
+}
+
+
+public function getCatIcon(){
+    $catid = $this->getData("title");
+    $catid = explode(",",$catid);
+
+    $iconurls = [];
+
+    foreach ($catid as $id) {
+        $category = $this->cataRepo->get($id);
+
+        $iconurls[] = $category->getData("thumbnail");
+        
+    }
+    return $iconurls; 
+
+
+
+}
+
+
+
+}
+?>
