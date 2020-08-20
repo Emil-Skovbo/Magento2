4c5af00b10c1976de70522b1389038b9fa3c3e80
Emil-Skovbo <emilskovbo18@gmail.com>
2020-08-20 12:33:53 +0200

    update for upload

--- app/code/Ereactor/MyModule/Controller/Adminhtml/Category/Thumbnailimage/Upload.php
+++ app/code/Ereactor/MyModule/Controller/Adminhtml/Category/Thumbnailimage/Upload.php
@@ -36,7 +36,7 @@ class Upload extends \Magento\Backend\App\Action
             $category = $this->catRepo->get(38);
             $category->setCustomAttribute('thumbnail', $urlPath);
             $this->catRepo->save($category);
-
+            
             
             $result['cookie'] = [
                 'name' => $this->_getSession()->getName(),
@@ -46,8 +46,10 @@ class Upload extends \Magento\Backend\App\Action
                 'domain' => $this->_getSession()->getCookieDomain(),
             ];
         } catch (\Exception $e) {
+            error_log("execute Exception error_log");
             $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
         }
+        error_log("upload error_log");
         return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
     }
 }
