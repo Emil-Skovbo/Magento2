ce684c0722d100e711dfc01718c37a4f15854664
Emil-Skovbo <emilskovbo18@gmail.com>
2020-08-24 16:19:41 +0200

    multi choice option

--- app/code/Ereactor/CategoryIconModule/Controller/Adminhtml/Category/Thumbnailimage/Upload.php
+++ app/code/Ereactor/CategoryIconModule/Controller/Adminhtml/Category/Thumbnailimage/Upload.php
@@ -27,7 +27,7 @@ class Upload extends \Magento\Backend\App\Action
         try {
             $result = $this->imageUploader->saveFileToTmpDir('thumbnail');
             $urlPath = $result["url"];
-            $category = $this->catRepo->get(0);
+            $category = $this->catRepo->get(38);
             error_log($urlPath . " saved");
             $category->setCustomAttribute('thumbnail', $urlPath);
             $this->catRepo->save($category);
