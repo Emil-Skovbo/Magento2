e43e9951f4c9259d04328cb5b565ab7afc006774
Emil-Skovbo <emilskovbo18@gmail.com>
2020-08-26 09:43:31 +0200

    multi choice option

--- app/code/Ereactor/CategoryIconModule/Block/CurrentCategory.php
+++ app/code/Ereactor/CategoryIconModule/Block/CurrentCategory.php
@@ -1,11 +1,13 @@
 <?php
 namespace Ereactor\CategoryIconModule\Block;
+
+use Magento\Framework\Registry;
  
 class CurrentCategory extends \Magento\Framework\View\Element\Template
 {
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Framework\Registry $registry,
+        Registry $registry,
         array $data = []
     ) {
         $this->registry = $registry;
@@ -15,8 +17,9 @@ class CurrentCategory extends \Magento\Framework\View\Element\Template
     /* $categoryId as category id */
     public function getCategory(){
         try {
-            error_log(print_r($this->registry->registry('current_category'), true));
-            return $this->registry->registry('current_category');
+            //error_log(print_r($this->registry->registry('current_category'), true));
+            $category = $this->registry->registry('current_category');
+            return $category;
         } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
             return ['response' => 'Category Not Found'];
         }
