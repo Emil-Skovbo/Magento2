4648c3c446a9e537414217dcf9d84641455b297f
Emil-Skovbo <emilskovbo18@gmail.com>
2020-08-26 11:42:38 +0200

    multi choice option

--- app/code/Ereactor/CategoryIconModule/Block/Widget/CategoryData.php
+++ app/code/Ereactor/CategoryIconModule/Block/Widget/CategoryData.php
@@ -47,7 +47,7 @@ class CategoryData extends Template implements BlockInterface
             'img' => $categoryid->getCustomAttribute('thumbnail')->getValue(),
             //gets the url to the category we are linking to
             'url' => $categoryname->getUrl(),
-            'testurl' => "",
+            'testid' => $this->getCurrentCategory()->getId(),
             'testurl2' => $this->getData("category_id")
         );
     }
