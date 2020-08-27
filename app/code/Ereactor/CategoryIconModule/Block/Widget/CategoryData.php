f8947773b68d36ec1bcee9012512afdff341ce90
Emil-Skovbo <emilskovbo18@gmail.com>
2020-08-27 10:38:02 +0200

    fixing IconModule

--- app/code/Ereactor/CategoryIconModule/Block/Widget/CategoryData.php
+++ app/code/Ereactor/CategoryIconModule/Block/Widget/CategoryData.php
@@ -44,7 +44,7 @@ class CategoryData extends Template implements BlockInterface
         $catinfo[] = array(
             'name' => $categoryname->getName(),
             //gets the url to the uploaded img
-            'img' => getThumbnailImageUrl(),
+            'img' => $this->getThumbnailImageUrl(),
             //$categoryid->getCustomAttribute('thumbnail')->getValue(),
             //gets the url to the category we are linking to
             'url' => $categoryname->getUrl(),
